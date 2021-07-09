b8ef51db990852802f597c664aea5ca26f574ac7
carlschader <carlschader@gmail.com>
2021-07-09 10:47:54 -0600

    using commit-testing now

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+// import '../styles/globals.css';
+
+import colors from '../styles/colors.json';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: colors
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+      <CssBaseline>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CssBaseline>
+    </>
+  );
 }
 
 export default MyApp
